d72dd0dab350adb6e8be6427696f50d1d81a28f0
Mohsina-Zaman <mohsina1zaman@gmail.com>
2026-05-09 20:30:11 -0400

    dont cause multiple ticks

--- main.js
+++ main.js
@@ -539,7 +539,6 @@ function update() {
         }
         if (!players.player2.hasHitSideSpecial && players.player2.isUsingSideSpecial) {
             lungePush(this, players.player2, players.player, 'swordatkthird');
-            players.player2.hasHitSideSpecial = true;
         }
         if (cursors.left.isDown) {
             if (!players.player2.isUsingSideSpecial) {
